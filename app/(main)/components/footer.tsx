b2b26079066ca082b2ae112d0a2c2c7627052078
Arina <arinasmakh@gmail.com>
2026-08-19 23:52:52 +0300

    changed footer

--- app/(main)/components/footer.tsx
+++ app/(main)/components/footer.tsx
@@ -1,60 +1,76 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Mail, MapPin, Building, Phone } from "lucide-react";
 import "@/app/(auth)/components/footer.css";
 
 export default function Footer() {
   return (
     <footer className="Footer-footer">
       <div className="Footer-Content">
-
-        {/* Колонка 1: Логотип и краткое описание */}
+        {/* Колонка 1: Логотип + описание */}
         <div className="Footer-Col Footer-Col-Logo">
           <div className="Footer-Logo">
             <Image
               className="Footer-LogoImg"
               src="/footer/Logo-Transparent.svg"
               alt="ClusterLab Logo"
-              width={40}
-              height={40}
+              width={64}
+              height={64}
               priority
             />
             <Link href="/">
               <span className="Footer-LogoText">ClusterLab</span>
             </Link>
           </div>
+          <p className="Footer-Description">
+            Платформа для точного земледелия<br />
+            на основе спутниковых данных
+          </p>
         </div>
 
         {/* Колонка 2: Документы */}
         <div className="Footer-Col">
+          <h4 className="Footer-Title">Документы</h4>
           <nav className="Footer-Links">
             <Link href="/privacy">
-              <p>Согласие на обработку данных</p>
+              <span>Согласие на обработку данных</span>
             </Link>
             <Link href="/offer">
-              <p>Договор оферты</p>
+              <span>Договор оферты</span>
             </Link>
           </nav>
         </div>
 
         {/* Колонка 3: Контакты */}
         <div className="Footer-Col">
-          <p className="Footer-Title">Контакты</p>
-          <div className="Footer-Info">
-            <p><a href="mailto:clusterlab@yandex.ru">clusterlab.ru@gmail.com</a></p>
-            <p>198334, Санкт-Петербург, Добровольцев ул., д. 44, Лит. А, пом. 3-Н, офис 11-2</p>
+          <h4 className="Footer-Title">Контакты</h4>
+          <div className="Footer-Contacts">
+            <a href="mailto:clusterlab.ru@gmail.com" className="Footer-ContactItem">
+              <Mail size={16} />
+              <span>clusterlab.ru@gmail.com</span>
+            </a>
+            <div className="Footer-ContactItem">
+              <MapPin size={16} />
+              <span>Санкт-Петербург, ул. Добровольцев, д. 44</span>
+            </div>
           </div>
         </div>
 
         {/* Колонка 4: Реквизиты */}
         <div className="Footer-Col">
-          <p className="Footer-Title">Реквизиты</p>
-          <div className="Footer-Info">
-            <p>ООО «КластерЛаб»</p>
-            <p>ИНН: 7807402857</p>
+          <h4 className="Footer-Title">Реквизиты</h4>
+          <div className="Footer-Requisites">
+            <div className="Footer-ContactItem">
+              <Building size={16} />
+              <span>ООО «КластерЛаб»</span>
+            </div>
+            <div className="Footer-ContactItem">
+              <span className="Footer-RequisiteLabel">ИНН:</span>
+              <span>7807402857</span>
+            </div>
           </div>
         </div>
-
       </div>
 
       {/* Нижняя плашка */}
