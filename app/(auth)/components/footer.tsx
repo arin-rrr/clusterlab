1a191661e0aa13e2c542e4d4d565d099bcf43821
Arina <arinasmakh@gmail.com>
2026-08-24 21:59:37 +0300

    perfect footer

--- app/(auth)/components/footer.tsx
+++ app/(auth)/components/footer.tsx
@@ -1,38 +1,16 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import "@/app/(main)/components/footer.css";
+import "@/app/(main)/components/auth-footer.css";
+
 export default function Footer() {
   return (
-    <footer className="Footer-footer">
-      <div className="Footer-Logo">
-        <Image
-          className="Footer-LogoImg"
-          src="/footer/Logo-Transparent.svg"
-          alt="ClusterLab Logo"
-          width={50}
-          height={100}
-          priority
-        />
-        <Link href="/">
-          <p className="Footer-LogoText">ClusterLab</p>
-        </Link>
-      </div>
-      <div className="Footer-Mail">
-        <Image
-          className="Footer-Mail-Img"
-          src="/footer/Mail.svg"
-          alt="Mail Icon"
-          width={25}
-          height={20}
-          priority
-        />
-        <a className="Footer-MailText">clusterlab.ru@gmail.com</a>
-      </div>
-      <nav className="Footer-Pages">
-        <p>Согласие на обработку персональных данных</p>
-        <p>Договор оферты</p>
+    <footer className="AuthFooter">
+      <nav className="AuthFooter-Links">
+        <a href="/privacy">Согласие на обработку персональных данных</a>
+        <a href="/offer">Договор оферты</a>
       </nav>
+      <a href="mailto:clusterlab.ru@gmail.com" className="AuthFooter-Mail">
+        clusterlab.ru@gmail.com
+      </a>
     </footer>
   );
 }
