520fcf57ca6c1116e9d5f444a1ddba8b90420910
Arina <arinasmakh@gmail.com>
2026-08-25 23:07:10 +0300

    changed footer

--- app/(auth)/components/footer.tsx
+++ app/(auth)/components/footer.tsx
@@ -5,8 +5,12 @@ export default function Footer() {
   return (
     <footer className="AuthFooter">
       <nav className="AuthFooter-Links">
-        <a href="/privacy">Политика обработки персональных данных</a>
-        <a href="/offer">Публичная оферта</a>
+        <a href="/documents/privacy-policy.pdf" target="_blank" rel="noopener noreferrer" className="Footer-topLink">
+          Политика обработки персональных данных
+        </a>
+        <a href="/documents/oferta.pdf" target="_blank" rel="noopener noreferrer" className="Footer-topLink">
+          Публичная оферта
+        </a>
       </nav>
       <a href="mailto:clusterlab.ru@gmail.com" className="AuthFooter-Mail">
         clusterlab.ru@gmail.com
