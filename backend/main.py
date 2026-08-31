959cc71d94fac5d30f9fd1ec7dc6299be5e838e1
Arina <arinasmakh@gmail.com>
2026-09-01 01:49:46 +0300

    changes

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,6 @@ app = FastAPI(
     title='ClusterLab',
     version='0.1.0'
 )
-
-app.include_router(users.router)
-app.include_router(fields.router)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
@@ -19,6 +15,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(users.router)
+app.include_router(fields.router)
+
+
 
 @app.get('/')
 async def root():
