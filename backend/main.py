2f47dda1778a58c3a4755db4b55cc2fa208bb9f1
Arina <arinasmakh@gmail.com>
2026-09-01 01:41:16 +0300

    changes

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,9 @@ app.include_router(fields.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000",
+                   "https://clusterlab.vercel.app/",
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
