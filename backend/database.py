7b385b18f4d4391ccfcbc5731b9876662459ae3d
Arina <arinasmakh@gmail.com>
2026-09-01 02:14:09 +0300

    changes

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL='postgresql+asyncpg://neondb_owner:npg_oWXjymT9za3F@ep-floral-bird-za7jd8bb-pooler.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+DATABASE_URL='postgresql+asyncpg://neondb_owner:npg_oWXjymT9za3F@ep-floral-bird-za7jd8bb-pooler.c-2.eu-west-2.aws.neon.tech/neondb'
 
 # Создаём Engine
 async_engine = create_async_engine(DATABASE_URL, echo=True)
