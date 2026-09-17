9dc18a14abc86f67c4174cedc986ffe276df1842
Arina <arinasmakh@gmail.com>
2026-09-17 20:41:35 +0300

    IRS

--- app/(auth)/sign_in/page.tsx
+++ app/(auth)/sign_in/page.tsx
@@ -25,7 +25,7 @@ export default function Sign_InPage() {
     formData.append("password", password);
 
     try {
-      const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+      const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://89.223.125.216:8000";
       const response = await fetch(`${API_URL}/users/token`, {
         // Замени на свой URL
         method: "POST",
--- backend_deploy/backend/main.py
+++ backend_deploy/backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://clusterlab.vercel.app",
+                   "https://clusterlab-ers.ru",
                    "https://clusterlab.site",
                    ],
     allow_credentials=True,
